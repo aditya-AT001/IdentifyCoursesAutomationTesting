e4df9906a0c80259560cd110bb8c5bc8e9110b37
Yadav <2479662@cognizant.com>
2026-04-16 12:02:55 +0530

    updated webcoursepage

--- pages/WebCoursePage.ts
+++ pages/WebCoursePage.ts
@@ -7,12 +7,7 @@ class CourseList{
     private selectEng:Locator;
     private selectBegin:Locator;
     private viewBtn:Locator;
-    private courses:Locator;
-    private firstCourseTitle:Locator;
-    private firstRating:Locator;
-
-
-
+   
     constructor(page:Page){
         this.page=page;
         this.lanfilter=this.page.locator("//div[text()='Language']");
@@ -20,9 +15,7 @@ class CourseList{
         this.levelFilter=this.page.locator('//div[text()="Level"]');
         this.selectBegin=this.page.locator("//span[text()='Beginner']");
         this.viewBtn=this.page.locator('button[data-testid="filter-view-button"]');
-        this.courses=this.page.locator(".cds-CommonCard-title.css-6ecy9b");
-        this.firstCourseTitle=this.page.locator("span.css-wo322s");
-        this.firstRating=this.page.locator('span[aria-hidden="true"]',{hasText:'4.4'});
+        
     }
     async filterLanandLevel(){
         await this.lanfilter.click();
